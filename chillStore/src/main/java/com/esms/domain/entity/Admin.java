7784dbc52c023cbdefdfd75d3df670e323bc7295
RAY\namng <namnguyen123456cm@gmail.com>
2025-06-01 12:11:40 +0700

    update package format mvc2 and vai thứ linh tin khác

--- chillStore/src/main/java/com/esms/domain/entity/Admin.java
+++
@@ -1,4 +0,0 @@
-package com.esms.domain.entity;
-
-public class Admin {
-}
---
+++ chillStore/src/main/java/com/esms/service/impl/CustomerServiceImpl.java
@@ -0,0 +1,44 @@
+package com.esms.service.impl;
+
+
+import com.esms.exception.EmailAlreadyUsedException;
+import com.esms.model.dto.RegisterDto;
+import com.esms.model.entity.Customer;
+import com.esms.repository.CustomerRepository;
+import com.esms.service.CustomerService;
+import com.esms.util.MapperUtils.CustomerMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    @Autowired
+    private final CustomerRepository customerRepository;
+    private final CustomerMapper customerMapper;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomerServiceImpl(CustomerRepository customerRepository, CustomerMapper customerMapper, PasswordEncoder passwordEncoder) {
+        this.customerRepository = customerRepository;
+        this.customerMapper = customerMapper;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public void register(RegisterDto dto) {
+        customerRepository.findByEmail(dto.getEmail()).ifPresent(customer -> {
+            throw new EmailAlreadyUsedException("Email " + dto.getEmail() + "đã tồn tại chọn cái khác đeeeee");
+        });
+    if (dto.getPassword().equals(passwordEncoder.encode(dto.getPassword()))) {
+        throw new IllegalArgumentException("mật khẩu méo chinh xác!");
+    }
+        Customer entity = customerMapper.toEntity(dto);
+
+        String rawPassword = dto.getPassword();
+        String hashedPassword = passwordEncoder.encode(rawPassword);
+        entity.setPassword(hashedPassword);
+
+        customerRepository.save(entity);
+    }
+}

794ecfa455197777645301618bfa30ea592e8566
RAY\namng <namnguyen123456cm@gmail.com>
2025-06-14 21:18:12 +0700

    update category ID tự fix thứ tự

--- chillStore/src/main/java/com/esms/repository/CategoryRepository.java
+++ chillStore/src/main/java/com/esms/repository/CategoryRepository.java
@@ -2,6 +2,7 @@ package com.esms.repository;
 
 import com.esms.model.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findByNameContainingIgnoreCase(String keyword);
 
     Optional<Category> findById(Integer id);
+    @Query("SELECT MAX(c.id) FROM Category c")
+    Integer findMaxId();
 }

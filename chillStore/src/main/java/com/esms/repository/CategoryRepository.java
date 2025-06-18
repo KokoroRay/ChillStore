9477c84178c02678da95d9c4fbff7436e5e90705
RAY\namng <namnguyen123456cm@gmail.com>
2025-06-18 23:18:15 +0700

    backout code voucher edit

--- chillStore/src/main/java/com/esms/repository/CategoryRepository.java
+++ chillStore/src/main/java/com/esms/repository/CategoryRepository.java
@@ -1,8 +1,10 @@
 package com.esms.repository;
 
 import com.esms.model.entity.Category;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -16,4 +18,6 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     Optional<Category> findById(Integer id);
     @Query("SELECT MAX(c.id) FROM Category c")
     Integer findMaxId();
+
+    Page<Category> findAll(Pageable pageable);
 }

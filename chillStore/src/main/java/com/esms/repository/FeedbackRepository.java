3ede0c1bcc063e790534265dce7885ff3873f87d
Truong Thi Vu Ha <hattvce181839@fpt.edu.vn>
2025-06-19 01:28:38 +0700

    updateStaff UI

---
+++ chillStore/src/main/java/com/esms/repository/FeedbackRepository.java
@@ -0,0 +1,8 @@
+/** package com.esms.repository;
+
+import com.esms.model.entity.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
+
+}  */

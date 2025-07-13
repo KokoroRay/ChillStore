bfdf2b28523eea04ae679d6e27e43da7712e8ec3
Huynh Le Cong Bien <BienHLCCE181207@fpt.edu.vn>
2025-07-13 23:04:31 +0700

    Update UI

--- chillStore/src/main/java/com/esms/controller/HomeController.java
+++ chillStore/src/main/java/com/esms/controller/HomeController.java
@@ -12,6 +12,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -60,5 +61,10 @@ public class HomeController {
 
         return "home";
     }
+
+    @GetMapping ("/about-us")
+        public String aboutUs(){
+        return"about-us";
+        }
 
 }
